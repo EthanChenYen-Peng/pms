0085897e8d4d2c42e867101e66965f64ba9d60dc
EthanChenYen-Peng <ypcethan@gmail.com>
2021-02-02 17:01:19 +0800

    Feature: Add uniqueness constriant on project title

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :content, presence: true
 
   default_scope { order(created_at: :desc) }
--- spec/features/projects/createing_projects_spec.rb
+++ spec/features/projects/createing_projects_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature 'Users can create project' do
-
   context 'locale: en' do
     scenario 'with valid inputs' do
       visit new_project_path
@@ -23,6 +22,17 @@ RSpec.feature 'Users can create project' do
       expect(page).to have_content 'Project has not been created.'
       expect(page).to have_content "Title can't be blank"
       expect(page).to have_content "Content can't be blank"
+    end
+
+    scenario 'with duplicate title' do
+      FactoryBot.create(:project, title: 'project1')
+      visit new_project_path
+      fill_in 'project[title]', with: 'project1'
+      fill_in 'project[content]', with: 'blog content'
+      click_button 'Create Project'
+
+      expect(page).to have_content 'Project has not been created.'
+      expect(page).to have_content 'Title has already been taken'
     end
   end
 
@@ -46,7 +56,18 @@ RSpec.feature 'Users can create project' do
 
       expect(page).to have_content '專案創建失敗'
       expect(page).to have_content '標題不能為空'
-      expect(page).to have_content '內容不能為空' 
+      expect(page).to have_content '內容不能為空'
+    end
+
+    scenario 'with duplicate title' do
+      FactoryBot.create(:project, title: 'project1')
+      visit new_project_path(locale: 'zh-TW')
+      fill_in 'project[title]', with: 'project1'
+      fill_in 'project[content]', with: 'blog content'
+      click_button 'Create Project'
+
+      expect(page).to have_content '專案創建失敗'
+      expect(page).to have_content '標題已經存在'
     end
   end
 end
