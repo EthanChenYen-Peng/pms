9f96649117c74ccd270c7575615a5dbf7089037d
EthanChenYen-Peng <ypcethan@gmail.com>
2021-02-12 12:37:15 +0800

    Add User model tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'username and email should be unique' do
+    User.create(username: 'Ethan', email: 'ethan@gmail.com', password: 'asdf')
+    user = User.new(username: 'Ethan', email: 'ethan@gmail.com', password: 'adsf')
+    user.save
+
+    expect(user.errors.any?).to be(true)
+    expect(user.errors[:username]).to match_array(['has already been taken'])
+    expect(user.errors[:email]).to match_array(['has already been taken'])
+  end
 end
